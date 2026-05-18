da17724f09b62b713d344a128f8ce0e615005f5a
paulamarcos-immune <paula.marcos@immune.institute>
2026-05-18 11:11:07 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -95,7 +95,7 @@ const MatrixLoader = () => {
           <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
           <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
         </svg>
-        <h1 className="text-xl font-black text-emerald-400 tracking-widest uppercase animate-pulse">Cargando IMMUNE...</h1>
+        <h1 className="text-xl font-black text-emerald-400 tracking-widest uppercase animate-pulse">Cargando Immune Connect...</h1>
       </div>
     </div>
   );
